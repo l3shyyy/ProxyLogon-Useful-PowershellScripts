ec7b8542342914a38bb7097e33efd9a04eff4a80
l3shy <engelmanjaxson@gmail.com>
2021-03-18 19:18:17 -0400

    Update PCALAudit.ps1

--- PCALAudit.ps1
+++ PCALAudit.ps1
@@ -1,1 +1,1 @@
-﻿Get-ADUser -LDAPFilter "(pwdLastSet=0)" | Select SamAccountName,distinguishedName | Export-CSV "$env:temp\ChangePasswordAtNextLogon.csv" -notypeinformation
+﻿Get-ADUser -LDAPFilter "(pwdLastSet=0)" | Select SamAccountName,distinguishedName | Export-CSV "$home\desktop\ChangePasswordAtNextLogon.csv" -notypeinformation
