363959e986a004ee80c82287f93300b2fc947894
l3shy <engelmanjaxson@gmail.com>
2021-06-17 09:42:31 -0400

    Update Get-AdUser.ps1

--- Get-AdUser.ps1
+++ Get-AdUser.ps1
@@ -1,1 +1,3 @@
 Get-ADUser -Filter * -Property whenCreated, whenChanged | Export-Csv -Path "$home\desktop\AdUsers.csv" -NoTypeInformation
+#or to the temp directory: 
+Get-ADUser -Filter * -Property whenCreated, whenChanged | Export-Csv -Path "C:\Temp\Updated-Users.csv" -NoTypeInformation
